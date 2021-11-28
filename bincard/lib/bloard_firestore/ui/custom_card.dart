67b3f08854517b94a7bfba634e20f46940c9b6c9
PhyoWin1993 <mr.phyo.1571993mm@gmail.com>
2021-11-28 23:49:37 +0630

    updtedd

--- bincard/lib/bloard_firestore/ui/custom_card.dart
+++ bincard/lib/bloard_firestore/ui/custom_card.dart
@@ -3,7 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:http/http.dart';
+
 import 'package:intl/intl.dart';
 
 class CustomCard extends StatelessWidget {
